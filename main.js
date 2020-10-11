587e677812aaa27d3e17c16ad89b03af47815c12
João Flavo Costa Teixeira Filho <72360863+Joaoflavo@users.noreply.github.com>
2020-10-11 05:19:03 -0300

    Add files via upload
    Códigos gerados do curso de introdução ao JavaScript, foi feita pagina com alguns alertas em JS

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var nome = "  Autor do site/Aluno: João Flavo De La Guerra -- J°@° 2@2@";
+var idade = 37;
+var nascido = 1982;
+alert ("Bem vindo a meu Projeto Javascript! desafio: Digital innovation one-Programação para internet com JavaScript-Programação para internet com Javascipt-JavaScript é uma das mais importantes linguagens front-end, e nesse curso você entenderá o porquê disso e como trabalhar com ela " +  nome);
+alert ( nome + "  nascido em  " + nascido + ", é tem "  + idade + "  anos de praia! Trabalha com TI à mais de 18 anos,  na área de Suporte Técnico.  Cargo atual Analista Técnico  de campo Responsável Senior  (field technician), da Empresa Ohana Informática.  ");
+
+
+    var a = prompt("Digite seu nome Por favor:","Nome");
+    document.write("<strong>Bom dia, " + a + " <br /></strong>");
+    document.write("<p><strong>Estamos trabalhando no código ele será JavaScript & Bons estudos!</strong></p>");
